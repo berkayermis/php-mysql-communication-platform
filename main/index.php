726617d4f7db3160de04e25914b3814933dd4a10
Berkay <67913214+berkayermis@users.noreply.github.com>
2021-05-29 03:05:35 +0300

    Existing files
    Connection to MySQL and some dummy datas which will be deleted.

---
+++ main/index.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Term Project</title>
+    <link rel="stylesheet" href="main.css">
+	<script src="https://kit.fontawesome.com/4f74b84bed.js" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+</head>
+<body>
+
+	<div>
+		<img class="school-logo" src="../icons/school-logo.png" alt="">
+	</div>
+
+	  <div id="container">
+		<!-- Cover Box -->
+		<div class="form-wrap">
+			<div class="tabs">
+				<h3 class="login-tab"><a class="active" href="#login-tab-content">Login</a></h3>
+				<h3 class="signup-tab"><a  href="#signup-tab-content">Sign Up</a></h3>
+			</div><!--.tabs-->
+	
+			<div class="tabs-content">
+				<div id="login-tab-content" class="active">
+					<form id="signInForm" class="login-form" action="index.php" method="post">
+						<input name="username" type="text" class="input" id="user_login" autocomplete="off" placeholder="Username">
+						<input name="password" type="password" class="input" id="user_pass" autocomplete="off" placeholder="Password">
+						<input type="checkbox" class="checkbox" id="remember_me">
+						<label for="remember_me">Remember me</label>
+	
+						<input id="realSignIn" type="submit" class="button" name="login" value="Login">
+					</form>
+					<div class="help-text">
+						<p><a href="#">Forget your password?</a></p>
+					</div>
+				</div>
+
+				<div id="signup-tab-content">
+					<form class="signup-form" action="index.php" method="post">
+						<input type="text" class="input" id="user_name" name="user_name" autocomplete="off" placeholder="Username">
+						<input type="text" class="input" id="fname" name="user_fname" autocomplete="off" placeholder="First Name">
+						<input type="text" class="input" id="lname" name="user_lname" autocomplete="off" placeholder="Last Name">
+						<input type="password" class="input" id="user_pass" name="user_pass" autocomplete="off" placeholder="Password">
+						<label class="option-class">Secretary
+							<input type="radio" checked="checked" <?php if (isset($radio) && $radio=="secretary") echo "checked";?> name="radio" value="secretary">
+							<span class="checkmark"></span>
+						</label>
+						<label class="option-class">Instructor
+							<input type="radio" name="radio" <?php if (isset($radio) && $radio=="instructor") echo "checked";?> value="instructor" >
+							<span class="checkmark"></span>
+						</label>
+						<label class="option-class">Student
+							<input type="radio" name="radio" <?php if (isset($radio) && $radio=="student") echo "checked";?> value="student">
+							<span class="checkmark"></span>
+						</label>
+						<input onclick="signupFunction()" name="register" type="submit" class="button" value="Sign Up">
+					</form><!--.login-form-->
+					<!-- <div class="help-text">
+						<p>By signing up, you agree to our</p>
+						<p><a href="#">Terms of service</a></p>
+					</div> -->
+				</div><!--.signup-tab-content-->
+			</div><!--.tabs-content-->
+		</div><!--.form-wrap-->
+		
+		
+	  <div class="icons">
+		<!-- <a href="https://www.facebook.com"><i class="fab fa-facebook-square"></i></a>
+	  	<a href=""><i class="fab fa-instagram"></i>	</a> -->
+			<ul class="icon-container">
+				<li class="icon-item"><a href="https://www.facebook.com/medipoluniversitesi"><img src="../icons/facebook.png" alt=""></a></li>
+				<li class="icon-item"><a href="https://www.instagram.com/medipolunv/"><img src="../icons/instagram.png" alt=""></a></li>
+				<li class="icon-item"><a href="https://www.linkedin.com/school/medipoluniversitesi/"><img src="../icons/linkedin.png" alt=""></a></li>
+				<li class="icon-item"><a href="https://twitter.com/medipolunv"><img src="../icons/twitter.png" alt=""></a></li>
+			</ul>
+	  </div>
+
+	  <script>
+		  function signupFunction(){
+			  alert("Your account was sent to confirmation!")
+		  }
+
+		jQuery(document).ready(function($) {
+		tab = $('.tabs h3 a');
+
+		tab.on('click', function(event) {
+			event.preventDefault();
+			tab.removeClass('active');
+			$(this).addClass('active');
+
+			tab_content = $(this).attr('href');
+			$('div[id$="tab-content"]').removeClass('active');
+			$(tab_content).addClass('active');
+	});
+});
+		  
+	</script>
+
+<?php
+require_once('../config.php');
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+if(isset($_POST['register'])){
+	$usern = $_POST['user_name'];
+	$firstn = $_POST['user_fname'];
+	$lastn = $_POST['user_lname'];
+	$passwrd = $_POST['user_pass'];
+	$user_type = test_input($_POST['radio']);
+
+	$query = "INSERT INTO user (username,fname,lname,user_pass,user_role) VALUES (?,?,?,?,?)";
+	$statement = mysqli_prepare($conn,$query);
+	mysqli_stmt_bind_param($statement,'sssss',$usern,$firstn,$lastn,$passwrd,$user_type);
+	mysqli_stmt_execute($statement);
+	print(mysqli_stmt_error($statement) . "\n");
+	mysqli_stmt_close($statement);
+
+	echo $usern . " " . $firstn . " ". $lastn . " " . $passwrd . " " . $user_type;
+}
+
+if(isset($_POST['login'])){
+	$user_login = $_POST['username'];
+	$passwrd_login = $_POST['password'];
+	
+	$sql = "SELECT username,user_pass,user_role FROM user";
+	$result = mysqli_query($conn,$sql);
+	if(mysqli_num_rows($result)>0){
+		while($row = mysqli_fetch_assoc($result)){
+			if($row['username'] == $user_login && $row['user_pass'] == $passwrd_login){
+				if($row['user_role']=="secretary"){
+					header('Location: ../secretary/secretary.php');
+					exit;
+				}
+				else if($row['user_role']=="instructor"){
+					header("Location: ../instructor/instructor.html");
+					exit;
+				}
+				else if($row['user_role']=="student"){
+					header("Location: ../student/student.html");
+					exit;
+				}
+				else{
+					echo "error";
+				}
+			}
+		}
+	}
+}
+
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+  }
+
+// $sql = "INSERT INTO user (username,fname,lname,user_pass,user_role) VALUES ('berkayermis','Berkay','Ermiş','123456','student')";
+
+// if (mysqli_query($conn, $sql)) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+// }
+
+mysqli_close($conn);
+?>
+
+</body>
+</html>
---
+++ secretary/secretary.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Secretary</title>
+    <link rel="stylesheet" href="secretary.css">
+    <link rel="stylesheet" href="../parent.css">
+    <script src="https://kit.fontawesome.com/4f74b84bed.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+  <div>
+    <ul>
+      <li>        
+        <nav>
+        <a href="#course"><img class="icon-logo-width" src="../icons/school-logo.png" alt=""></a><hr>
+        <a href="#profile"><i class="far fa-user"></i></a>
+        <a href="#course"><i class="fas fa-book"></i></a>
+        <a href="#message"><i class="far fa-envelope"></i></a>
+        <a href="../main/index.php"><i class="fas fa-sign-out-alt"></i></a>
+      </nav>
+    </li>
+
+      <li class="flex-item">
+          <div class= 'container2'> 
+
+           <section id= 'profile'>
+             <h1>Profile</h1>
+           </section>
+           
+           <section id='course'> 
+
+           <div id="idcreate-course" class="modal">
+                <span onclick="document.getElementById('idcreate-course').style.display='none'" class="close" title="Close Modal">&times;</span>            
+                <form class="form" method="post" action="secretary.php">
+                  <label for="">Course Code</label> <br> 
+                  <input type="number" min="1" max="100000" value="Course code" name="course_code"><br>
+                  <label for="">Course Name</label>
+                  <input type="text" id="cname" name="cname" placeholder="Course name" >
+                  <label for="course-type">Course Type</label> <br>
+                <select name="course_type" id="course-type">
+                  <option value="Mandatory">Mandatory</option>
+                  <option value="Elective">Elective</option>
+                </select> <br>
+                <label for="instructor">Instructor</label> <br>
+                <select name="instructor" id="instructor">
+                  <?php 
+                require_once('../config.php');
+                $ins_conn = mysqli_connect($servername, $username, $password,$db);
+            
+                if (!$ins_conn) {
+                  die("Connection failed: " . mysqli_connect_error());
+                }
+                else{
+                  echo "error";
+                }
+                  $ins_sql = "SELECT id,fname,lname,user_role FROM user WHERE user_role='instructor' ";
+                  $ins_result = mysqli_query($ins_conn,$ins_sql);
+                  if(mysqli_num_rows($ins_result)>0){
+                    while($ins_row = mysqli_fetch_assoc($ins_result)){
+                      $instructor_id = $ins_row['id'];
+                      echo "<option>" . $ins_row['fname'] . " " . $ins_row['lname'] . "</option>";
+                    }
+                    echo "</select>";
+                  }
+                  else{
+                    echo "error";
+                  }
+                  ?>
+                <label for="date">Date</label>
+                <input type="datetime-local" id="date" name="date"> <br>
+                <button onclick="myFunction()" name="create_course_button" type="submit">Create Course</button>
+                </form>
+            </div>
+
+             <div style="display: inline-block;">
+              <div>
+                <a onclick="document.getElementById('idcreate-course').style.display='block'" href="#course" class="create-course-btn" ><p>Create Course</p></a>
+              </div>
+
+              <div class="secretary-dropdown">
+                <button href="#course" class="create-course-btn2">Department</button>
+                <div class="secretary-dropdown-content">
+                <a href="#course">Computer Engineering</a>
+                <a href="#course">Electrics Engineering</a>
+                <a href="#course">Biomedical Engineering</a>
+                <a href="#course">Industrial Engineering</a>
+                <a href="#course">Civil Engineering</a>
+                </div>
+              </div>
+              
+              <div class="secretary-dropdown2">
+                <button href="#course" class="create-course-btn3">Download</button>
+                <div class="secretary-dropdown-content2">
+                <a href="../files/all-courses.xls" download="">.xls</a>
+                <a href="../files/all-courses.pdf" download>.pdf</a>
+                </div>
+              </div>
+
+             </div>
+
+              <table id="research-table">
+                <caption>
+                  <h2>All Courses</h2>
+                </caption>
+                <thead>
+                  <tr>
+                    <th>Course Code</th>
+                    <th>Course Name</th>
+                    <th>Type</th>
+                    <th>Instructor</th>
+                    <th>Time</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+          </section>
+           
+          <section id= 'message'>
+            <h1>Message</h1>
+           </section>
+         </div>
+
+      </li>
+    </ul>
+  </div>
+  
+
+  <script>
+    function myFunction() {
+      alert("Course was created succesfully!");
+    }
+
+    function deleteFunction() {
+    if (confirm("Are you sure to delete?")) {
+      alert("Course was deleted!");
+  }
+    else {
+    }
+}
+    </script>
+
+    <?php
+    require_once('../config.php');
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password,$db);
+
+    // Check connection
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    function delete_data($data){
+      $sql_delete = "DELETE FROM course WHERE id='$data' ";
+    
+      if (mysqli_query($conn, $sql_delete)) {
+         echo "Record deleted successfully";
+    } else {
+         echo "Error deleting record: " . mysqli_error($conn);
+    }
+    }
+
+// $sql = "INSERT INTO course (code,course_name,course_type,instructor_id,date_time) VALUES ('COE3167930','Physics','Mandatory',6,'2021/01/12 02:53')";
+
+// if (mysqli_query($conn, $sql)) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+// }
+
+$sql = "SELECT id,code,course_name,course_type,instructor_id,date_time FROM course";
+$result = mysqli_query($conn, $sql);
+ 
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    $data_id = $row['id'];
+    echo "<tr>" . 
+    "<td>" . $row['code']. "</td>" . 
+    "<td>" . $row['course_name']. "</td>" . 
+    "<td>" . $row['course_type']. "</td>" . 
+    "<td>" . $row['instructor_id']. "</td>" . 
+    "<td>" . $row['date_time']. "</td>" .
+    "<td>" . "<a onclick=\"delete_data($data_id)\" href=\"#course\">" . "<i class=\"fas fa-trash-alt\">" . "</i>" .  "</a>" . "</td>" . "</tr>";
+  }
+  echo "</tbody>" . "</table>";
+} else {
+  echo "0 results";
+}
+
+if(isset($_POST['create_course_button'])){
+	$cc_course_code = $_POST['course_code'];
+	$cc_course_name = $_POST['cname'];
+	$cc_course_type = $_POST['course_type'];
+	$cc_course_datetime = $_POST['date'];
+  $cc_course_instructor_id = $instructor_id;
+
+	$query = "INSERT INTO course (code,course_name,course_type,instructor_id,date_time) VALUES (?,?,?,?,?)";
+	$statement = mysqli_prepare($conn,$query);
+	mysqli_stmt_bind_param($statement,'sssis',$cc_course_code,$cc_course_name,$cc_course_type,$cc_course_instructor_id,$cc_course_datetime);
+	mysqli_stmt_execute($statement);
+	print(mysqli_stmt_error($statement) . "\n");
+	mysqli_stmt_close($statement);
+}
+ 
+
+mysqli_close($conn);
+
+    ?>
+
+ 
+
+</body>
+</html>
