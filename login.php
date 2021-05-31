ce5a58083ee65e405e4d5a648fbaeb57fe45189c
Berkay <67913214+berkayermis@users.noreply.github.com>
2021-05-31 03:40:09 +0300

    Student redirection

--- login.php
+++ login.php
@@ -14,12 +14,14 @@ if (!$conn) {
 if(isset($_POST['login'])){
 	$user_login = $_POST['username'];
 	$passwrd_login = $_POST['password'];
-	$sql = "SELECT id,username,user_pass,user_role FROM user";
+	$sql = "SELECT id,username,fname,lname,user_pass,user_role FROM user";
 	$result = mysqli_query($conn,$sql);
 	if(mysqli_num_rows($result)>0){
 		while($row = mysqli_fetch_assoc($result)){
 			if($row['username'] == $user_login && $row['user_pass'] == $passwrd_login){
 				$_SESSION['username'] = $row['username'];
+				$_SESSION['fname'] = $row['fname'];
+				$_SESSION['lname'] = $row['lname'];
 				$_SESSION['user_id'] = $row['id'];
 				$_SESSION['user_role'] = $row['user_role'];
 				if($row['user_role']=="secretary"){
@@ -31,7 +33,7 @@ if(isset($_POST['login'])){
 					exit;
 				}
 				else if($row['user_role']=="student"){
-					header("Location: student/student.php");
+					header("Location: student/student.php#profile");
 					exit;
 				}
 				else{
